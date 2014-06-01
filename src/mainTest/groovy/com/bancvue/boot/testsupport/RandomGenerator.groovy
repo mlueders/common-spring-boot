49829d1afd9295b24b92c55d1a8a067126632ef2
Mike Lueders <mike@roadkill.org>
2014-05-31 19:24:51 -0500

    [PPOC-380] added RandomGenerator to support random builders

---
+++ src/mainTest/groovy/com/bancvue/boot/testsupport/RandomGenerator.groovy
@@ -0,0 +1,70 @@
+package com.bancvue.boot.testsupport
+
+import org.fluttercode.datafactory.impl.DataFactory
+
+public class RandomGenerator {
+
+	private static Random random = new Random();
+	private static DataFactory df = new DataFactory();
+
+	static {
+		init(getSeed());
+	}
+
+	private static long getSeed() {
+		return random.nextLong();
+	}
+
+	public static void init(long seed) {
+		random = new Random(seed);
+		df = new DataFactory();
+		df.randomize(random.nextInt());
+	}
+
+	public static DataFactory getDataFactory() {
+		return df;
+	}
+
+	public static long id() {
+		long id = random.nextInt();
+		if (id < 0) {
+			id *= -1;
+		} else if (id == 0) {
+			id = 1
+		}
+		return id;
+	}
+
+	public static int number() {
+		return random.nextInt();
+	}
+
+	public static String text(int length) {
+		return df.getRandomText(length);
+	}
+
+	public static String zipCode() {
+		return df.getNumberText(5);
+	}
+
+	public static String phoneNumber() {
+		return df.getNumberText(3) + "-" + df.getNumberText(3) + "-" + df.getNumberText(4);
+	}
+
+	public static String numericText(int digits) {
+		return df.getNumberText(digits);
+	}
+
+	public static String address() {
+		return df.getNumberText(5) + " " + df.getRandomText(8) + " " + item("St", "Rd", "Ave");
+	}
+
+	public static <T> T item(List<T> items) {
+		return df.getItem(items);
+	}
+
+	public static <T> T item(T... items) {
+		return df.getItem(Arrays.asList(items));
+	}
+
+}
