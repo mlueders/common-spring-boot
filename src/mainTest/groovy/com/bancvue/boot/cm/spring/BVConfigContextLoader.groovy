3844b4b128293bd772528889883969745a4c432c
Mike Lueders <mike@roadkill.org>
2014-06-03 10:09:38 -0500

    [PPOC-380] updated BVConfigContextLoader to initialize bvconfig properties if not already initialized

--- src/mainTest/groovy/com/bancvue/boot/cm/spring/BVConfigContextLoader.groovy
+++ src/mainTest/groovy/com/bancvue/boot/cm/spring/BVConfigContextLoader.groovy
@@ -1,5 +1,6 @@
 package com.bancvue.boot.cm.spring
 
+import com.bancvue.cm.ConfigurationRuleUtil
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.test.SpringApplicationContextLoader
 
@@ -7,6 +8,7 @@ public class BVConfigContextLoader extends SpringApplicationContextLoader {
 
 	@Override
 	protected SpringApplication getSpringApplication() {
+		ConfigurationRuleUtil.useBVConfiguration()
 		SpringApplication app = new SpringApplication();
 		app.addInitializers(new PropertySourceInitializer());
 		return app;
